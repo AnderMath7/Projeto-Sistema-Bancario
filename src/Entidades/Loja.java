d98047c38e939c55bec22819c2d3efb667b2554a
AnderMath7 <matheusanderson7@gmail.com>
2024-09-23 20:59:50 -0300

    criação classe Loja

--- src/Entidades/Loja.java
+++ src/Entidades/Loja.java
@@ -1,5 +1,21 @@
 package Entidades;
 
-public class Loja {
+import java.util.ArrayList;
 
+public class Loja {
+	private int codigoLoja = 01;
+	private ArrayList<Funcionario> funcionarios = new ArrayList<>();
+	Loja(){
+		
+	}
+	public int getCodigoLoja() {
+		return codigoLoja;
+	}
+	public ArrayList<Funcionario> getFuncionarios() {
+		return funcionarios;
+	}
+	public void adicionaFuncionarios(Funcionario funcionario) {
+		this.funcionarios.add(funcionario);
+	}
+	
 }
