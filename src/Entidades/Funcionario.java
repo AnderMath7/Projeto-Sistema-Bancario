96eae04d888f40635455540bfffe99ee158583e7
AnderMath7 <matheusanderson7@gmail.com>
2024-09-23 20:59:25 -0300

    criação classe Funcionarios

--- src/Entidades/Funcionario.java
+++ src/Entidades/Funcionario.java
@@ -1,5 +1,20 @@
 package Entidades;
 
-public class Funcionario {
-
+public class Funcionario extends Thread{
+	private int id = 0;
+	private int codigoFuncionario = 02;
+	Funcionario(int id){
+		this.id = id;
+	}
+	
+	public void run(){
+		
+	}
+	
+	public long getId() {
+		return id;
+	}
+	public int getCodigoFuncionario() {
+		return codigoFuncionario;
+	}
 }
