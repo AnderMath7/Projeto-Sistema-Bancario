1b3dca210a999fc290abf911c21a32cc95af78ee
AnderMath7 <matheusanderson7@gmail.com>
2024-09-23 13:21:45 -0300

    Criação da entidade conta

---
+++ src/Entidades/Conta.java
@@ -0,0 +1,15 @@
+package Entidades;
+
+public class Conta {
+	private double saldo = 0;
+	Conta(){
+		
+	}
+	public double getSaldo() {
+		return saldo;
+	}
+	public void setSaldo(double saldo) {
+		this.saldo = saldo;
+	}
+	
+}
